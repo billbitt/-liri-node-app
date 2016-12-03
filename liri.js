7040dfded1051089f7b45a3306fe13e97f54150a
bill bittner <bittner.w@gmail.com>
2016-12-02 19:36:09 -0800

    added spotify functionality

--- liri.js
+++ liri.js
@@ -4,6 +4,9 @@ var keys = require("./keys.js");
 var twitter = require("twitter");
 var client = new twitter(keys.twitterKeys);
 
+//spotify
+var spotify = require("spotify");
+
 //get user input
 var input = process.argv;
 var command = input[2];
@@ -15,24 +18,43 @@ function myTweets(){
     console.log("");
     //log all the tweets
     client.get('statuses/user_timeline', {count: "20"}, function(error, tweets, response) {
+        //check for error
         if(error) {
-            return console.log("an error occurred finding your tweets");
-        } else {
-            //console.log(tweets);  // The favorites. 
-            for (var i = 0; i < tweets.length; i++){
-                console.log("tweet #" + (i + 1));
-                console.log(tweets[i].text);
-                console.log("");
-            };
-            //console.log(response);  // Raw response object. 
+            console.log("An error occurred finding your tweets.");
+            console.log("error: " + error);
+            return;
+        };
+        //display the tweets 
+        for (var i = 0; i < tweets.length; i++){
+            console.log("tweet #" + (i + 1));
+            console.log(tweets[i].text);
+            console.log("");
         };
     });
-
 };
 
 //get song information from spotify
 function spotifySong(songName){
-    //do stuff
+    //log a blank space
+    console.log("");
+    //display the song information
+    spotify.search({ type: 'track', query: songName }, function(error, response) {
+        //check for error
+        if (error) {
+            console.log("An error occurred finding your song.");
+            console.log("error: " + error);
+            return;
+        }
+        //console.log(response.tracks.items[0]);
+        //display Artist(s)
+        console.log("Artist: " + response.tracks.items[0].artists[0].name); //note this shoudl print all in the artists array
+        //display the song's name
+        console.log("Song Name: " + response.tracks.items[0].name);
+        //display a preview link of the song from Spotify
+        console.log("Preview URL: " + response.tracks.items[0].preview_url);
+        //display the album that the song is from
+        console.log("Album: " + response.tracks.items[0].album.name);
+    });
 };
 
 //get song information from OMDB
